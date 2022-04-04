c632dc3e2410d58fcbbb86dcd9719585df06de9a
GZ4003 <you@example.com>
2022-04-04 12:02:44 -0300

    final upload

---
+++ src/constants/categories.js
@@ -0,0 +1,26 @@
+export const CATEGORIES = [
+    {
+        id: 1,
+        name: 'Tomates',
+        color: '#ff585d',
+        description: 'tomates muy ricos'
+    },
+    {
+        id: 2,
+        name: 'Lechuga',
+        color: '#00BD9D',
+        description: 'Variedad de lechugas'
+    },
+    {
+        id: 3,
+        name: 'Cebollas',
+        color: '#ae2573',
+        description: 'Cebollas comunes,de color'
+    },
+    {
+        id: 4,
+        name: 'Papas',
+        color: '#6b4c4c',
+        description: 'Papas muy ricas'
+    },
+]
